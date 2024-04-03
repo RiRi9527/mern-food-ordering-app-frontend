128f42cf6be2be44fca66e61ca6383e8030d7b2d
Hao Zhou <hzhou129527@gmail.com>
2024-04-03 19:38:44 -0400

    adding view order features

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -8,10 +8,22 @@ const MobileNavLinks = () => {
   return (
     <>
       <Link
+        to="/order-status"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Order Status
+      </Link>
+      <Link
         to="/user-profile"
         className="flex bg-white items-center font-bold hover:text-orange-500"
       >
         User Profile
+      </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        My restaurant
       </Link>
       <Button
         onClick={() => logout()}
--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,14 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useGetMyRestaurantOrders,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
+import OrderItemCard from "@/components/OrderItemCard";
+import { Tabs } from "@/components/ui/tabs";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+import { TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { useState } from "react";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isCreateLoading } =
@@ -12,14 +17,51 @@ const ManageRestaurantPage = () => {
   const { updateRestaurant, isLoading: isUpdateLoading } =
     useUpdateMyRestaurant();
 
+  const { orders } = useGetMyRestaurantOrders();
+
   const isEditing = !!restaurant;
 
+  const [selected, setSelected] = useState("Orders");
+
   return (
-    <ManageRestaurantForm
-      restaurant={restaurant}
-      onSave={isEditing ? updateRestaurant : createRestaurant}
-      isLoading={isCreateLoading || isUpdateLoading}
-    />
+    <Tabs defaultValue="orders">
+      <TabsList className="space-x-5 mb-10">
+        <TabsTrigger
+          value="orders"
+          onClick={() => setSelected("Orders")}
+          className={` ${
+            selected === "Orders" ? "underline text-orange-500" : ""
+          }`}
+        >
+          Order
+        </TabsTrigger>
+        <TabsTrigger
+          value="manage-restaurant"
+          onClick={() => setSelected("manage-restaurant")}
+          className={` ${
+            selected !== "Orders" ? "underline text-orange-500" : ""
+          }`}
+        >
+          Manage Restaurant
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent
+        value="orders"
+        className="space-y-5 bg-gray-50 p-10 rounded-lg"
+      >
+        <h2 className="text-2xl font-bold">{orders?.length} active orders</h2>
+        {orders?.map((order) => (
+          <OrderItemCard order={order} />
+        ))}
+      </TabsContent>
+      <TabsContent value="manage-restaurant">
+        <ManageRestaurantForm
+          restaurant={restaurant}
+          onSave={isEditing ? updateRestaurant : createRestaurant}
+          isLoading={isCreateLoading || isUpdateLoading}
+        />
+      </TabsContent>
+    </Tabs>
   );
 };
 
